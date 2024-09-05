aa8ed15b2a9201bb993637e47c4dfea4c775f1f4
JuliSem <julia.sem295@yandex.ru>
2024-09-05 23:14:55 +0300

    install and setting django-debug-toolbar

--- app_store/urls.py
+++ app_store/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from debug_toolbar.toolbar import debug_toolbar_urls
+
+from app_store.settings import DEBUG
 
 
 urlpatterns = [
@@ -23,3 +26,6 @@ urlpatterns = [
     path('', include('main.urls', namespace='main')),
     path('catalog/', include('goods.urls', namespace='catalog')),
 ]
+
+if DEBUG:
+    urlpatterns += debug_toolbar_urls()
