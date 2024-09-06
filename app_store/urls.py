738d2c623b857aee3219031757c94dd8fd5f6eef
JuliSem <julia.sem295@yandex.ru>
2024-09-06 16:10:44 +0300

    registered simple tag for categories

--- app_store/urls.py
+++ app_store/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import include, path
-from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf.urls.static import static
 
-from app_store.settings import DEBUG
+from app_store import settings
 
 
 urlpatterns = [
@@ -27,5 +28,7 @@ urlpatterns = [
     path('catalog/', include('goods.urls', namespace='catalog')),
 ]
 
-if DEBUG:
+if settings.DEBUG:
     urlpatterns += debug_toolbar_urls()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
