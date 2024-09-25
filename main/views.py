ae84c4973c91ce62512f0b52ce14e3a824b33db6
JuliSem <julia.sem295@yandex.ru>
2024-09-25 22:50:35 +0300

    create based class views for apps

--- main/views.py
+++ main/views.py
@@ -1,24 +1,38 @@
 from lib2to3.fixes.fix_input import context
-from django.http import HttpResponse
-from django.shortcuts import render
-
-from goods.models import Categories
+from django.views.generic import TemplateView
 
 
+class IndexView(TemplateView):
+    template_name = 'main/index.html'
 
-def index(request):
-
-    context = {
-        'title': 'Home - Главная',
-        'content': 'Магазин мебели HOME'
-    }
-    return render(request, 'main/index.html', context)
+    def get_context_data(self, **kwargs: context):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Home - Главная'
+        context['content'] = 'Магазин мебели HOME'
+        return context
 
 
-def about(request):
-    context = {
-        'title': 'Home - О нас',
-        'content': 'О нас',
-        'text_on_page': 'Текст о том, какой хороший у нас классный магазин и хороший товар.'
-    }
-    return render(request, 'main/about.html', context)
+class AboutView(TemplateView):
+    template_name = 'main/about.html'
+
+    def get_context_data(self, **kwargs: context):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Home - О нас'
+        context['content'] = 'О нас'
+        context['text_on_page'] = 'Текст о том, какой хороший у нас классный магазин и хороший товар.'
+        return context
+
+# def index(request):
+#     context = {
+#         'title': 'Home - Главная',
+#         'content': 'Магазин мебели HOME'
+#     }
+#     return render(request, 'main/index.html', context)
+# 
+# def about(request):
+#     context = {
+#         'title': 'Home - О нас',
+#         'content': 'О нас',
+#         'text_on_page': 'Текст о том, какой хороший у нас классный магазин и хороший товар.'
+#     }
+#     return render(request, 'main/about.html', context)
